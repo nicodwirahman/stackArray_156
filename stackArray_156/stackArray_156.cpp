c8caad8922c9ddb11ab329a127abf16fdb1ddd43
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-06-16 11:10:51 +0700

    memasukkan element pop

--- stackArray_156/stackArray_156.cpp
+++ stackArray_156/stackArray_156.cpp
@@ -17,13 +17,21 @@ public:
 		string element;
 		getline(cin, element);
 
-		if(top ==4) {//step 1
-			cout << "Number of data exceeds the limit." << endl;
+		if (top == 4) {//step 1
+			cout << "Number of data exceds the limit." << endl;
 			return;
-	}
+		}
 
 		top++;//step2
 		stack_array[top] = element;//step 3
 		cout << endl;
 		cout << element << "ditambahkan(pushed)" << endl;
+
+	}
+	void pop() {
+		if (empty()) {//step 1
+			cout << "\nStack is empty.cannon pop." << endl;//1.a
+			return;//1.b
+	}
+
 };
